9efe3846c1b0e4d749733e4c3c345fbd35c5a1d9
watsnav <navneetwats@yahoo.in>
2020-04-21 10:09:32 +0000

    update qotd

--- quotsi/sh/runqotd.sh
+++ quotsi/sh/runqotd.sh
@@ -9,7 +9,7 @@ function check_connectivity() {
 #fi
 if check_connectivity; then
 	if !ls /run/postgresql > /dev/null 2>&1; then
-		echo "1"
+		#echo "1"
 		if mkdir /run/postgresql >/dev/null 2>&1; then 
 			chown -R 1000:1000 /run/postgresql
 		else
@@ -20,14 +20,14 @@ if check_connectivity; then
 		fi
 		./updateqotd.sh -s
 	elif !ls /run/postgresql/*lock* > /dev/null 2>&1; then
-		echo "2"
+		#echo "2"
 		own=stat -c %u /run/postgresql >/dev/null 2>&1 
 		if [ "$own" != "1000" ]; then
 			chown -R 1000:1000 /run/postgresql
 		fi
 		./updateqotd.sh -s
 	else
-		echo "3"
+		#echo "3"
 		./updateqotd.sh
 	fi
 else
