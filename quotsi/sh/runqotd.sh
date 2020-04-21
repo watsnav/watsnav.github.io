04c59505b774e43337e416f0675c5d364bbf0238
watsnav <navneetwats@yahoo.in>
2020-04-21 12:08:48 +0000

    update qotd

--- quotsi/sh/runqotd.sh
+++ quotsi/sh/runqotd.sh
@@ -8,11 +8,17 @@ function check_connectivity() {
 	#exit 1
 #fi
 if check_connectivity; then
+	echo "WebService is accessible!"
 	if ls /run/postgresql/*lock* > /dev/null 2>&1; then
 		#database is running
+		echo "Database is running!"
+		pushd .
+		cd /home/watsnav/scripts/www/watsnav.github.io/quotsi/sh
 		./updateqotd.sh
+		popd
 	elif ls /run/postgresql > /dev/null 2>&1; then
 		#database is not running but requirements are fullfilled
+		echo -n "Database is not running, attempting to start.."
 		own=stat -c %u /run/postgresql >/dev/null 2>&1 
 		if [ "$own" != "1000" ]; then
 			chown -R 1000:1000 /run/postgresql
@@ -20,6 +26,7 @@ if check_connectivity; then
 		./updateqotd.sh -s
 	else 
 		#requirements not fulfilled
+		echo -n "Database is not running, attempting to start.."
 		mkdir /run/postgresql >/dev/null 2>&1
 		chown -R 1000:1000 /run/postgresql
 		./updateqotd.sh -s
