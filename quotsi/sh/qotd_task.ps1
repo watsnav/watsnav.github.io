84321155952217b4049e27acaec1c5ae50f0ca05
watsnav <navneetwats@yahoo.in>
2020-05-08 09:54:30 +0530

    update qotd

--- quotsi/sh/qotd_task.ps1
+++ quotsi/sh/qotd_task.ps1
@@ -1,8 +1,5 @@
 ﻿net start postgresql-x64-12
-echo $LASTEXITCODE
-if($LASTEXITCODE) {
-    D:\prjs\www\watsnav.github.io\genquote.exe
-    git add D:\prjs\www\watsnav.github.io\
-    git commit -m "update qotd"
-    git push
-}
+D:\prjs\www\watsnav.github.io\genquote.exe
+git add D:\prjs\www\watsnav.github.io\
+git commit -m "update qotd"
+git push
